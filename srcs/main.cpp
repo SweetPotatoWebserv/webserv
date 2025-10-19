a251abc723eb733fe5ea79eef369bef4dd390f35
Yutosaki <sasayu8190@gmail.com>
2025-10-19 21:06:19 +0900

    🚧 fix code for success linter and formatter check

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -4,7 +4,6 @@
 int main() {
   if (true) { // <-- This should fail clang-format (bad spacing)
     int *p = 0;
-    int x = *p; // <-- This should fail clang-tidy (null pointer dereference)
     std::cout << "Test" << x << std::endl;
   }
   return 0;
