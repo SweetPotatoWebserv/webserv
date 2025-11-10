d2f2af0994be3715f0ee4f35efc73530757ff94d
yuu-ux <yuugaehara@gmail.com>
2025-11-10 17:07:21 +0900

    Use pragma once in String header

--- src/core/String.h
+++ src/core/String.h
@@ -1,10 +1,7 @@
-#ifndef STRING_H
-#define STRING_H
+#pragma once
 
 #include <string>
 #include <vector>
 
 std::vector<std::string> split(const std::string& buffer,
                                const std::string& sep = " ");
-
-#endif
