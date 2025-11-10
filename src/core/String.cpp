2ddc6d98c0d830c8b611f12455f022d586416260
yuu-ux <yuugaehara@gmail.com>
2025-11-10 13:23:33 +0900

    :wrench: size_t を std::string::size_type に変更

--- src/core/String.cpp
+++ src/core/String.cpp
@@ -4,9 +4,9 @@ std::vector<std::string> split(const std::string& buffer,
                                const std::string& sep) {
     std::vector<std::string> res;
     std::string::size_type offset = 0;
-    size_t sep_len = sep.size();
+    std::string::size_type sep_len = sep.size();
     while (1) {
-        size_t pos = buffer.find(sep, offset);
+        std::string::size_type pos = buffer.find(sep, offset);
         if (pos == std::string::npos) {
             res.push_back(buffer.substr(offset));
             break;
