379e112f2133a87dfabef0673cbcd59c780fa41e
yuu-ux <yuugaehara@gmail.com>
2025-11-10 13:25:18 +0900

    :recycle: true で無限ループを回す

--- src/core/String.cpp
+++ src/core/String.cpp
@@ -5,7 +5,7 @@ std::vector<std::string> split(const std::string& buffer,
     std::vector<std::string> res;
     std::string::size_type offset = 0;
     std::string::size_type sep_len = sep.size();
-    while (1) {
+    while (true) {
         std::string::size_type pos = buffer.find(sep, offset);
         if (pos == std::string::npos) {
             res.push_back(buffer.substr(offset));
