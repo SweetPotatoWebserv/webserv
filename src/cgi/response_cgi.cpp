b12a658a081cc7f9090f0b02d70046c1ffca3f14
Yutosaki <sasayu8190@gmail.com>
2025-11-11 17:13:57 +0900

    ♻️  関数を切り分けたのでlinterのエラーを改善したはず

--- src/cgi/response_cgi.cpp
+++ src/cgi/response_cgi.cpp
@@ -18,12 +18,51 @@ const char* const LF_LF = "\n\n";
 const size_t LF_LF_LENGTH = 2;
 
 const char* const STATUS_HEADER_LOWER = "status";
-const char* const CONTETNT_TYPE_HEADER = "content-type";
+const char* const CONTENT_TYPE_HEADER = "content-type";
 const char* const LOCATION_HEADER_LOWER = "location";
 
 const int MIN_STATUS = 100;
 const int MAX_STATUS = 599;
 }  // namespace
+
+void parseCgiHeaderLine(const std::string& line, HttpResponse& response) {
+    std::string::size_type colon_pos = line.find(':');
+
+    if (colon_pos == std::string::npos || colon_pos == 0) {
+        return;  // 不正なヘッダー
+    }
+
+    std::string header_name = line.substr(0, colon_pos);
+    std::string header_value = line.substr(colon_pos + 1);
+
+    std::string::size_type value_start = header_value.find_first_not_of(" \t");
+    if (value_start != std::string::npos) {
+        header_value = header_value.substr(value_start);
+    } else {
+        header_value = "";
+    }
+
+    std::transform(header_name.begin(), header_name.end(), header_name.begin(),
+                   ::tolower);  // 大文字小文字の両方に対応するため(区別される)
+
+    if (header_name == STATUS_HEADER_LOWER) {
+        std::stringstream ss_status(header_value);
+        int parsed_status = 0;
+
+        if (ss_status >> parsed_status) {
+            if (parsed_status >= MIN_STATUS && parsed_status <= MAX_STATUS) {
+                response.status_code_ = parsed_status;
+            }
+        }
+        // else: 変換出来無い場合はデフォルト(200)のまま
+
+    } else if (header_name == CONTENT_TYPE_HEADER) {
+        response.header_.content_type_ = header_value;
+
+    } else if (header_name == LOCATION_HEADER_LOWER) {
+        response.location_ = header_value;
+    }
+}
 
 void parseCgiResponse(HttpResponse& response, const std::string& raw_output) {
     std::string::size_type pos = raw_output.find(LF_LF);
@@ -53,47 +92,9 @@ void parseCgiResponse(HttpResponse& response, const std::string& raw_output) {
         if (line.empty()) {
             continue;
         }
-
-        std::string::size_type colon_pos = line.find(':');
-
-        if (colon_pos == std::string::npos || colon_pos == 0) {
-            continue;
-        }
-
-        std::string header_name = line.substr(0, colon_pos);
-        std::string header_value = line.substr(colon_pos + 1);
-
-        std::string::size_type value_start =
-            header_value.find_first_not_of(" \t");
-        if (value_start != std::string::npos) {
-            header_value = header_value.substr(value_start);
-        } else {
-            header_value = "";
-        }
-
-        std::transform(
-            header_name.begin(), header_name.end(), header_name.begin(),
-            ::tolower);  // 大文字小文字の両方に対応するため(区別される)
-
-        if (header_name == STATUS_HEADER_LOWER) {
-            std::stringstream ss_status(header_value);
-            int parsed_status = 0;
-
-            if (ss_status >> parsed_status) {
-                if (parsed_status >= MIN_STATUS &&
-                    parsed_status <= MAX_STATUS) {
-                    response.status_code_ = parsed_status;
-                }
-            }
-            // else: 変換出来無い場合はデフォルト(200)のまま
-
-        } else if (header_name == CONTETNT_TYPE_HEADER) {
-            response.header_.content_type_ = header_value;
-
-        } else if (header_name == LOCATION_HEADER_LOWER) {
-            response.location_ = header_value;
-        }
+        parseCgiHeaderLine(line, response);
     }
+
     if ((response.status_code_ == HttpStatus::OK ||
          response.status_code_ == HttpStatus::Created) &&
         response.location_.empty() &&  // リダイレクトでもない
