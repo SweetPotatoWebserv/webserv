7fb1e4da8c4621c689d2969b6ebe084bb37d320d
Yutosaki <sasayu8190@gmail.com>
2025-11-11 14:30:52 +0900

    ➕ accessを使用して404と403のエラーに対応してから以前通りのhandlerの処理を行うようにした

--- src/cgi/handler_cgi.cpp
+++ src/cgi/handler_cgi.cpp
@@ -2,12 +2,14 @@
 
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
 #include <string>
 
+#include "../core/Common.h"
 #include "../http/HttpParser.h"
 #include "executor_cgi.h"
 #include "request_cgi.h"
@@ -18,35 +20,54 @@ CgiProcess::CgiProcess() { executor_ = new CgiExecutor(); }
 CgiProcess::~CgiProcess() { delete executor_; }
 
 bool CgiProcess::run(const HttpRequest& request, HttpResponse& response) {
-    // CgiEnvBuilderなどに移行したい(envpにしてexecveの時に渡せば他の環境変数は引き継がれないので削除する必要がない)
-    std::string script_path = request.request_target_.path_;
-    char** argv = createArgv(script_path);
-    char** envp = createEnvp(request);
-
-    // HttpRequestからstdin(リクエストボディ)の内容を取得
-    const std::string& request_body = request.body_;
-
-    std::string raw_output;
     try {
-        // CgiExecutorでスクリプトを実行
-        raw_output = executor_->execute(script_path, argv, envp, request_body);
-    } catch (const CgiExecutionException& e) {
-        // CgiExecutor(fork, pipe, execve, waitpid)でエラーが発生した場合
-        std::cerr << "CGI Execution failed: " << e.what() << "\n";
+        std::string script_path = request.request_target_.path_;
+        if (access(script_path.c_str(), F_OK) == -1) {
+            response.status_code_ = HttpStatus::NotFound;
+            response.body_ = "CGI script not found.";
+            return true;
+        }
 
-        // エラーレスポンスを生成
-        response.status_code_ = e.getStatusCode();
-        response.body_ = e.what();
+        if (access(script_path.c_str(), X_OK) == -1) {
+            response.status_code_ = HttpStatus::Forbidden;
+            response.body_ = "CGI script is not executable.";
+            return true;
+        }
+        // CgiEnvBuilderなどに移行したい(envpにしてexecveの時に渡せば他の環境変数は引き継がれないので削除する必要がない)
+        char** argv = createArgv(script_path);
+        char** envp = createEnvp(request);
+
+        // HttpRequestからstdin(リクエストボディ)の内容を取得
+        const std::string& request_body = request.body_;
+
+        std::string raw_output;
+        try {
+            // CgiExecutorでスクリプトを実行
+            raw_output =
+                executor_->execute(script_path, argv, envp, request_body);
+        } catch (const CgiExecutionException& e) {
+            // CgiExecutor(fork, pipe, execve, waitpid)でエラーが発生した場合
+            std::cerr << "CGI Execution failed: " << e.what() << "\n";
+
+            // エラーレスポンスを生成
+            response.status_code_ = e.getStatusCode();
+            response.body_ = e.what();
+
+            freeArray(argv);
+            freeArray(envp);
+            return true;
+        }
+
+        // CGIの生出力(stdoutから親proccessが読み取ったもの)をパースしてHttpResponseに格納
+        parseCgiResponse(response, raw_output);
 
         freeArray(argv);
         freeArray(envp);
+        return true;
+    } catch (const std::exception& e) {
+        std::cerr << "CgiProcess FATAL error: " << e.what() << "\n";
+
+        // エラーページすら生成できなかった、という意味でfalseを返す
         return false;
     }
-
-    // CGIの生出力(stdoutから親proccessが読み取ったもの)をパースしてHttpResponseに格納
-    parseCgiResponse(response, raw_output);
-
-    freeArray(argv);
-    freeArray(envp);
-    return true;
 }
