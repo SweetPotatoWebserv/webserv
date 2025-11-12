4af549325e2476fb20d8296fd452ed19e67eb8cd
yuu-ux <yuugaehara@gmail.com>
2025-11-12 14:33:42 +0900

    :wrench: search_header_field を追加
     Conflicts:
    	Makefile
    	src/core/Common.cpp
    	src/core/Common.h
    	src/http/HttpParser.h

--- src/core/String.h
+++ src/core/String.h
@@ -4,6 +4,11 @@
 #include <string>
 #include <vector>
 
+#include "../core/Common.h"
+
 std::vector<std::string> split(const std::string& buffer,
                                const std::string& sep = " ");
 std::string trim(const std::string& s);
+bool search_header_field(const std::string& request_message,
+                         const std::string& search_field,
+                         std::vector<std::string>& founded_field_vec);
