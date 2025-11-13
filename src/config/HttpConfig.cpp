cc84a99bcbd88386de48b5130c54d47ec7e9bdd8
Mori0113 <ruhakaforest.kaki.88@gmail.com>
2025-11-13 14:37:21 +0900

    :heavy_plus_sign:ErrorPageDirectiveにmapの機能を追加したためコンストラクタ追加 コンストラクタの追加

--- src/config/HttpConfig.cpp
+++ src/config/HttpConfig.cpp
@@ -2,6 +2,8 @@
 
 // ErrorPageDirective のコンストラクタ実装
 ErrorPageDirective::ErrorPageDirective(): override_status(-1) {}
+ErrorPageDirective::ErrorPageDirective(std::string t, int o) 
+    : target(t), override_status(o) {}
 
 // ListenDirective のコンストラクタ実装
 ListenDirective::ListenDirective() : port(DEFAULT_PORT), is_default_server(false) {}
