0f35ebf09c7c8926c4e7abae5c80a8774d8703dd
Mori0113 <ruhakaforest.kaki.88@gmail.com>
2025-11-13 15:02:28 +0900

    :heavy_plus_sign:コンストラクタの追加。 Httpconfig.hにあったものをHttpconfig.cppに移動しました。

--- src/config/HttpConfig.cpp
+++ src/config/HttpConfig.cpp
@@ -1,5 +1,14 @@
 #include "HttpConfig.h" // 自分のヘッダーをインクルード
 
+CommonConfig::CommonConfig()
+    : root_is_set(false),
+      autoindex_is_set(false),
+      autoindex(false),
+      upload_store_is_set(false),
+      redirect_is_set(false),
+      client_max_body_size(-1) // 制限なし
+{}
+
 // ErrorPageDirective のコンストラクタ実装
 ErrorPageDirective::ErrorPageDirective(): override_status(-1) {}
 ErrorPageDirective::ErrorPageDirective(std::string t, int o) 
