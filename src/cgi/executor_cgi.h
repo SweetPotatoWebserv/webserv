8b9c091d33371fcffc29c5241950841e11118bfc
Yutosaki <sasayu8190@gmail.com>
2025-11-15 14:14:48 +0900

    :heavy_plus_sign: executor_cgi.hがないとコンパイルが通らずlinterが落ちてしまうためcgi_base_processブランチから取ってきて追加する。実行部分はここのブランチを宛先として別のPRで追加予定

---
+++ src/cgi/executor_cgi.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <sys/types.h>
+
+#include <exception>
+#include <string>
+
+#include "../core/Common.h"
+
+class CgiExecutionException : public std::exception {
+   public:
+    CgiExecutionException(const std::string &message,
+                          HttpStatus::Code statusCode)
+        : message_(message), statusCode_(statusCode) {}
+    CgiExecutionException(const CgiExecutionException &other) throw()
+        : std::exception(other),
+          message_(other.message_),
+          statusCode_(other.statusCode_) {}
+    virtual ~CgiExecutionException() throw() {}
+
+    virtual const char *what() const throw() { return message_.c_str(); }
+    HttpStatus::Code getStatusCode() const { return statusCode_; }
+
+   private:
+    std::string message_;
+    HttpStatus::Code statusCode_;
+};
+
+class CgiExecutor {
+   public:
+    CgiExecutor();
+    ~CgiExecutor();
+
+    std::string execute(const std::string &scriptPath, char *const argv[],
+                        char *const envp[], const std::string &requestBody);
+
+   private:
+    pid_t pid_;
+    int pipeIn_[2];
+    int pipeOut_[2];
+
+    void executeChildProcess(const std::string &scriptPath, char *const argv[],
+                             char *const envp[]);
+    std::string readParentProcess(const std::string &requestBody);
+    static std::string getScriptDirectory(const std::string &scriptPath);
+    static std::string getScriptBasename(const std::string &scriptPath);
+};
