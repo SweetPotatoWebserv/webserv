9959b4a56ffa309fd494c188dbf4e827f6d38535
yuu-ux <yuugaehara@gmail.com>
2025-11-16 21:07:06 +0900

    :heavy_plus_sign: HttpDate を追加

---
+++ src/http/HttpDate.cpp
@@ -0,0 +1,18 @@
+#include "HttpDate.h"
+
+std::string HttpDate::getCurrentGMT() {
+    std::time_t now = std::time(NULL);
+    std::tm* gmt_time = std::gmtime(&now);
+    if (gmt_time == NULL) {
+        std::cerr << "getCurrentGMT Failed\n";
+        return "";
+    }
+
+    char buffer[BUFFER_SIZE];
+    if (!std::strftime(buffer, sizeof(buffer), "%a, %d %b %Y %H:%M:%S GMT",
+                       gmt_time)) {
+        std::cerr << "getCurrentGMT Failed\n";
+        return "";
+    }
+    return std::string(buffer);
+}
