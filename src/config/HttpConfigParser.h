738bd74d2abc2536946b25a610c647ca04c3aa9f
Mori0113 <ruhakaforest.kaki.88@gmail.com>
2025-11-17 12:33:18 +0900

    :recycle:MIN_OVERRIDE_STATUS_CODE,MAX_OVERRIDE_STATUS_CODEをHttpConfigParser.hに移動

--- src/config/HttpConfigParser.h
+++ src/config/HttpConfigParser.h
@@ -130,12 +130,8 @@ class HttpConfigParser {
     static const off_t BYTES_PER_MB = static_cast<off_t>(1024) * 1024;
     static const off_t BYTES_PER_GB = static_cast<off_t>(1024) * 1024 * 1024;
 
-    // 最大値のポート番号を定数化
-    static const int MAX_PORT_NUMBER = 65535;
-    // error pageの範囲
-    static const int MIN_ERROR_STATUS_CODE = 300;
-    static const int MAX_ERROR_STATUS_CODE = 599;
     static const int MIN_OVERRIDE_STATUS_CODE = 200;
+    static const int MAX_OVERRIDE_STATUS_CODE = 599;
 
     template <typename T>
     static bool parseCommonDirective(T& config, const std::string& token,
