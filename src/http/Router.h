869f79e46e0f40b19206f16b1c47001dd3dd7fcb
yuu-ux <yuugaehara@gmail.com>
2025-11-17 20:54:30 +0900

    :wrench: エラーページ解決用の関数を追加

--- src/http/Router.h
+++ src/http/Router.h
@@ -2,7 +2,6 @@
 #include "../config/HttpConfig.h"
 #include "HttpException.h"
 #include "HttpParser.h"
-#include "HttpResponse.h"
 #include "ResolveConfig.h"
 
 typedef struct RouteInfo {
@@ -15,10 +14,10 @@ class Router {
    public:
     explicit Router(const HttpConfig& config);
     RouteInfo route(const HttpRequest& request) const;
+    static const LocationConfig& find_location(const ServerConfig& server,
+                                               const std::string& path);
 
    private:
     const ServerConfig& find_server(const HttpRequest& request) const;
-    static const LocationConfig& find_location(const ServerConfig& server,
-                                               const std::string& path);
     HttpConfig config_;
 };
