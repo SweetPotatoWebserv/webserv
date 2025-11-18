80b53a62bae685ffed0afab4736ee9a4bb4afc28
Yutosaki <sasayu8190@gmail.com>
2025-11-18 19:01:57 +0900

    :heavy_plus_sign: query stringの読み込みを追加

--- src/cgi/request_cgi.h
+++ src/cgi/request_cgi.h
@@ -4,6 +4,7 @@
 
 #include "../http/HttpParser.h"
 
+std::string extractQueryString(const std::string& uri);
 char** createArgv(const std::string& script_path);
 char** createEnvp(const HttpRequest& request);
 void freeArray(char** arr);
