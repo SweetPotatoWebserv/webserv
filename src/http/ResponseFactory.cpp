34055ea6a4153b96f1a292e2a78fc4cb870d43bc
yuu-ux <yuugaehara@gmail.com>
2025-11-20 15:47:05 +0900

    :recycle: マジックナンバーを定数に置き換え

--- src/http/ResponseFactory.cpp
+++ src/http/ResponseFactory.cpp
@@ -257,8 +257,8 @@ HttpResponse ResponseFactory::response_redirect(const RouteInfo& route) {
     response.status_code_ = route.resolve_.redirect_.status;
     response.message_ = HttpStatus::reason(response.status_code_);
     // TODO Config で定義してる定数を Common に移動したらここも置き換える
-    if (response.status_code_ >= 302 &&  // NOLINT
-        response.status_code_ <= 308) {  // NOLINT
+    if (response.status_code_ >= MIN_REDIRECT_STATUS_CODE &&  // NOLINT
+        response.status_code_ <= MAX_REDIRECT_STATUS_CODE) {  // NOLINT
         response.location_ = route.resolve_.redirect_.target;
         response.header_.content_type_ = "text/html";
         std::stringstream oss;
