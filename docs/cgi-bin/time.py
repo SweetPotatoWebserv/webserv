bd93c91d31364b54c2e5c82ba8d03a72d85e119a
Mori0113 <ruhakaforest.kaki.88@gmail.com>
2025-11-26 13:42:17 +0900

    :recycle:ファイルの中に改行を追加

--- docs/cgi-bin/time.py
+++ docs/cgi-bin/time.py
@@ -3,3 +3,4 @@ import datetime
 print("Content-Type: text/html\r\n\r\n")
 print("<html><body><h1>CGI Time Check</h1>")
 print("<p>Current Time: {}</p></body></html>".format(datetime.datetime.now()))
+
