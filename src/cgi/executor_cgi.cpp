a0c145349f5df306a997bb11fe673654f932311d
Yutosaki <sasayu8190@gmail.com>
2025-11-26 22:25:24 +0900

    :recycle: 不要な初期化を削除

--- src/cgi/executor_cgi.cpp
+++ src/cgi/executor_cgi.cpp
@@ -90,10 +90,6 @@ CgiResult CgiExecutor::execute(const std::string &scriptPath,
     result.readFd = pipeOut_[0];
     result.writeFd = pipeIn_[1];
 
-    // -1をつけないと親で触る前にデストラクターで消されちゃう
-    pipeOut_[0] = -1;
-    pipeIn_[1] = -1;
-
     return result;
 }
 
